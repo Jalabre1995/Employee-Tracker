e7c8d98db6d4e084f3b9bce2d81ff31df6942cb0
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-02-21 14:16:19 -0500

    Employee-Tracker

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ function start(){
     .prompt({
         name: "addORRemove",
         type: "list",
-        message: "Would you like to [ADD] an employee or [REMOVE] an employee ?",
-        choices: ["ADD", "REMOVE"]
+        message: "Would you like to do ?",
+        choices: ["ADD", "REMOVE", "View All employees by Department", "View all employees by Manager", "View all employees", "Update Employees role", "Update Employees Manager"]
     })
     .then(function(answer) {
         //base on the answer, add or rmove employee///
@@ -38,9 +38,20 @@ function start(){
         }
         else if (answer.addORRemove === "REMOVE") {
             removeEmployee();
-        }else {
-            connection.end();
-        }
+        }else if(answer.addORRemove === "View All employees by Department"){
+            viewEmployeesDepart();
+        }else if( answer.addORRemove === "View all employees by Manager"){
+            viewManager();
+        }else if(answer.addORRemove === "View all employees"){
+            viewEmployees();
+        }else if (answer.addORRemove === "Update Employees role"){
+            updateEmpRole();
+        }else if(answer.addORRemove === "Update Employees Manager"){
+            updateManager();
+        }else connection.end();
+            
+        
+        
     });
 }
 
@@ -62,7 +73,7 @@ function addEmployee(){
             name:"role",
             type: "input",
             message: "What is their role?",
-            choices: ["Sales Lead", "Slaes Person", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead"]
+            choices: ["Sales Lead", "Sales Person", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead"]
         }
 
 
@@ -79,6 +90,7 @@ function addEmployee(){
             function(err){
                 if(err) throw err;
                 console.log("Your employee has been added!");
+                console.table(answer);
                 //re-prompt the user for if they want to add or remove///
                 start();
             }
@@ -110,10 +122,61 @@ connection.query("SELECT * FROM employee_list", function(err, results) {
                 type: "input",
                 message: "What is their role?",
                 choices: ["Sales Lead", "Slaes Person", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead"]
-        }
+        } 
     ]).then(function(answer){
         /////Remove the employee////
+        connection.query(
+            "INSERT INTO employee_list SET ?",
+            {
+                first_name: answer.first,
+                last_name: answer.last,
+                role_id: answer.role 
+            },
+            function(err) {
+                if (err) throw err;
+                console.log("Your employee was successfully removed!");
+                console.table(answer);
+                start();
+            }
+        )
         
     })
 })
+}
+
+///function to see all departments/////
+function viewEmployeesDepart(){
+    connection.query("SELECT * FROM department_list", function (err, answer) {
+        console.log("\n Departments Retreived from the Database \n");
+        console.table(answer);
+    });
+    start();
+};
+
+///function view all managers///
+function viewManager() {
+    connection.query("SELECT * FROM employee_list", function (err, answer) {
+        console.log("\n Departments Retreived from the Database \n");
+        console.table(answer);
+    })
+};
+
+////To view employees/////
+function viewEmployees() {
+    console.log("retrieving employees from database");
+    var emp = 
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title, deaprtment.name AS department, role.salary FROM employee "
+    connection.query (emp, function(err, answer) {
+        console.log("\n Employees retrieved from the Database \n");
+        console.table(answer);
+    });
+    start();
+}
+///function to update/////
+function updateEmpRole() {
+    let allemployes = [];
+    connection.query("SELECT * FROM employee_list", function(err, answer) {
+        console.log(answer);
+        
+    })
 }
